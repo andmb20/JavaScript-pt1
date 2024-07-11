ab2e98bcf62f93ae7ac82e6894220f39feafb29d
andre <andremorais0000@gmail.com>
2024-07-11 00:04:36 -0300

    Início de repositório JS + domínio plataforma GitHub

---
+++ arquivo.js
@@ -0,0 +1,10 @@
+/*
+    JavaScript pt1
+    Como executar o JavaScript?
+
+*/
+
+// Como de prache um famoso 'Hello World' para começarmos com o pé direito.
+console.log("Hello World")
+
+console.log("Bora pra cima desbravar JS")
