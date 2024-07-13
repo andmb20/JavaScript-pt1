cfa8fde4ee55b1a84d548d01c4375c6fc0a6ae93
andre <andremorais0000@gmail.com>
2024-07-13 19:58:44 -0300

    Adicionando arquivo functions.js descrevendo o que são as funções.

---
+++ functions.js
@@ -0,0 +1,10 @@
+/*
+    FUNCTIONS / FUNÇÕES - Trechos de código que é executado SOMENTE quando solicitado.
+        Podemos solicitar a função quantas vezes quisermos.
+*/
+
+function contaNaTela() {
+    console.log( 2 + 2 )
+}
+
+contaNaTela() //solicitando código da função contaNaTela()
