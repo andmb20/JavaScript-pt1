c8c3c393494b0b70e3a362ac3502de3df3a6d2e9
andre <andremorais0000@gmail.com>
2024-07-13 19:46:59 -0300

    incluindo if else e alguns operadores de comparação

---
+++ ifelse.js
@@ -0,0 +1,20 @@
+/*
+    IF / ELSE
+    IF (se) -> Se ISSO for Verddadeiro, faça ISSO!
+    Else (se NÃO) -> Se NÃO, faça ISSO.
+
+    Operadores de comparação
+    > maior que 
+    < menor que
+    == igual que
+*/
+
+const notaDeCorte = 7
+const notaDoAluno = 6
+
+if (notaDeCorte > notaDoAluno) {
+    console.log("PARABENS, Voce passou!!")
+}
+else {
+    console.log("REPROVADO")
+}
